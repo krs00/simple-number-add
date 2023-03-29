08172b718e80ab88051b41bde43af2d3f07c6a82
Kymani Stephens <kymanist00@gmail.com>
2023-03-29 15:30:57 -0700

    form will display user inputs

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
+// BUSINESS LOGIC
+
 function countUpBy(countBy, countTo){
     let Array = []
 
-    for (let index = 0; index <= countTo; index += countBy) {
+    for (let index = countBy; index <= countTo; index += countBy) {
         
         console.log(index)
         Array.push(index)
         console.log(Array)
+        return Array 
 
     }
 };
+
+
+
+// UI LOGIC
+// SELECT DOM NODES
+const countByDisplay = document.getElementById("display-countby")
+const countToDisplay = document.getElementById("display-countto")
+const arrayDisplay = document.getElementById("display-arr")
+const submitBtn = document.getElementById("submit")
+
+function runForm (event) {
+    event.preventDefault();
+
+    const countBy = document.getElementById("countby").value
+    const countTo = document.getElementById("countto").value
+
+    console.log(countBy);
+    displayVal(countBy, countTo);
+}
+function displayVal(countBy, countTo){ 
+    countByDisplay.innerText = `Count By Number: ${countBy}`
+    countToDisplay.innerText = `Count To Number: ${countTo}`
+}
+const form = document.getElementById("count-form");
+form.addEventListener("submit", runForm);
+
