5440a104c13dd860452c5c07cbc019b575ae37b7
Kymani Stephens <kymanist00@gmail.com>
2023-03-29 14:27:32 -0700

    countUpBy adds input into Array
    Co-authored-by: Brandon Morgan <github-Brandmorgan@gmail.com>

---
+++ script.js
@@ -0,0 +1,11 @@
+function countUpBy(countBy, countTo){
+    let Array = []
+
+    for (let index = 0; index <= countTo; index += countBy) {
+        
+        console.log(index)
+        Array.push(index)
+        console.log(Array)
+
+    }
+};
